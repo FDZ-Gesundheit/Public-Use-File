69ce6e71fa488082919e3e73fc42647711cbc27b
Lisa Kühnel <lisa.kuehnel@bfarm.de>
2024-08-26 09:37:39 +0200

    improve cleaning

--- helpers.py
+++ helpers.py
@@ -54,7 +54,7 @@ def clean_data(column_data, dt):
         column_data = column_data.astype('category')
     elif dt == "date":
         column_data = pd.to_datetime(column_data, format='%Y%m%d').dt.date
-        column_data.replace({np.nan: None}, inplace=True)
+        column_data = column_data.apply(lambda x: x if not pd.isnull(x) else None)
     elif dt == "year":
         column_data = pd.to_datetime(column_data, format='%Y').dt.year
     elif dt == "integer":
